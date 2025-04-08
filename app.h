3b4a6c935e06b3cef4d18518c8c41764eab5dde2
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-04-08 08:31:06 +0200

    Update app.h

--- app.h
+++ app.h
@@ -4,19 +4,22 @@
 #include "utils.h"
 #include "map.h"
 #include "entity.h"
+#include "item.h"
 
-#define PLAYER_VISION
+//#define PLAYER_VISION
 
 void SDL_ERR(int code);
 void *P_SDL_ERR(void *ptr);
-void Text_Renderer_C(SDL_Renderer *renderer, TTF_Font *font, i32 startX, i32 startY, i32 w_c, i32 h_c, char *c, SDL_Color textColor);
+void Text_Renderer_C(SDL_Renderer *renderer, TTF_Font *font, i32 startX, i32 startY, i32 w_c, i32 h_c, const char *c, SDL_Color textColor);
 void render_player(Entitiy *player);
-void player_input(SDL_Event *event, Entitiy* player, Entitiy_DA *entitis, Tile* map);
+void player_input(SDL_Event *event, Entitiy* player, Entitiy_DA *entitis, Item_DA *items, Tile* map);
 void render_stats(Entitiy *player);
 void render_monsters(Entitiy_DA *monsters, Entitiy *player);
 void render_messages(i32 startX, i32 startY, char* message);
 void render_map(Tile *map, Entitiy *player);
-void main_renderer(Entitiy* player, Entitiy_DA *monster, Tile *map);
-void event_user(Entitiy *player, Entitiy_DA *entitis, Tile* map);
+void main_renderer(Entitiy* player, Entitiy_DA *monster, Item_DA *items, Tile *map);
+void event_user(Entitiy *player, Entitiy_DA *entitis, Item_DA *items, Tile* map);
+void render_inventory(Item_DA *inventory);
+
 
 #endif
