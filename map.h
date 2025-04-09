75159053a6139d9afb1cc06b7494537bbb796177
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-04-09 06:11:29 +0200

    Update map.h

--- map.h
+++ map.h
@@ -9,6 +9,7 @@ typedef struct {
 	char ch;
 	SDL_Color color;
 	u8   isW;
+	u8   isV;
 	} Tile;
 	
 typedef struct {
@@ -21,6 +22,11 @@ typedef struct {
 #define MAP(map, x, y)      map[((x) % MAP_X) + ((y) % MAP_Y) * MAP_X]
 #define MAP_CH(map, x, y)   map[((x) % MAP_X) + ((y) % MAP_Y) * MAP_X].ch
 #define MAP_ISW(map, x, y)  map[((x) % MAP_X) + ((y) % MAP_Y) * MAP_X].isW
+#define MAP_ISV(map, x, y)  map[((x) % MAP_X) + ((y) % MAP_Y) * MAP_X].isV
+
+#define CLEAR_VISON_FIELD(map){for(u64 i = 0; i < MAP_X * MAP_Y; i++){map[i].isV = SDL_FALSE;}};
+
+
 
 
 Room create_room(i32 x, i32 y, i32 height, i32 width);
@@ -31,6 +37,7 @@ void add_walls_around_roads(Tile* map);
 void caved_part(Tile *map, i32 x, i32 y);
 void caved_map(Tile *map, f64 percantage);
 void generete_dungons(Tile *map, i32 minRooms, i32 maxRooms);
+
 Tile* init_map();
 
 
