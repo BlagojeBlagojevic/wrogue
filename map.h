74bb064a8530bd7ed599bc25e6191b90ca5a61d6
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-04-18 10:48:59 +0200

    Update map.h

--- map.h
+++ map.h
@@ -38,7 +38,11 @@ typedef struct {
 SDL_bool checkCollision(i32 x1, i32 y1, i32 w1, i32 h1, i32 x2, i32 y2, i32 w2, i32 h2);
 Room create_room(i32 x, i32 y, i32 height, i32 width);
 void add_room_to_map(Tile *map, Room room);
+void add_room_wall_rectangle(Tile *map, Room room);
+void add_room_wall_circle(Tile *map, Room room);
+void add_room_wall_blob(Tile *map, Room room);
 void add_room_wall_to_map(Tile *map, Room room);
+
 SDL_bool isDoor(Tile *map, Position pos);
 void add_doors(Tile *map);
 void connect_room_centers(Position centerOne, Position centerTwo, Tile* map, SDL_bool isDoorDis);
