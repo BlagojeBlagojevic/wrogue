81d593d3acf10bfc63b5a5fa7d02ccfa4b2cccfa
Blagoje Blagojevic <blagojeblagojevic562@gmail.com>
2025-05-19 09:40:45 +0200

    Update app.h

--- app.h
+++ app.h
@@ -8,21 +8,16 @@
 
 //#define PLAYER_VISION
 
-extern SDL_Texture* monstersTextures;
-extern SDL_Texture* groundTextures;
-extern SDL_Texture* wallTextures;
-extern SDL_Texture* poisonTextures;
-extern SDL_Texture* blightTextures;
-extern SDL_Texture* playerTextures;
-extern SDL_Texture* cloudTextures;
-extern SDL_Texture*      itemTextures;
+
 void init_texture();
  
 
 void SDL_ERR(int code);
 void *P_SDL_ERR(void *ptr);
+
 void Text_Renderer_C(SDL_Renderer *renderer, TTF_Font *font, i32 startX, i32 startY, i32 w_c, i32 h_c, const char *c, SDL_Color textColor);
 void render_player(Entitiy *player);
+void render_interpolated(Entitiy *ent);
 void player_input(SDL_Event *event, Entitiy* player, Entitiy_DA *entitis, Item_DA *items, Tile* map);
 void render_stats(Entitiy *player);
 void render_monsters(Entitiy_DA *monsters, Entitiy *player, Tile *map);
